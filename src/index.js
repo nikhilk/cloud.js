98545e6b4b4d209246aec0fb4664a496e8e4ebb6
Nikhil Kothari <nikhilko@google.com>
2014-11-11 21:24:28 -0800

    Stub file for main module file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// index.js
+//
+
+/**
+ * Runs the application.
+ * @param {String} path  The root directory that the application is located in.
+ * @param {Function} require  The module loader associated with the application.
+ */
+function run(path, require) {
+}
+
+exports.run = run;
